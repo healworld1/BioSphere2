9980e6b0b08d01f29b21672199b2be8e46152adc
zozer firehood <firehood.co@gmail.com>
2018-03-11 11:39:30 -0400

    hopefully fixed road

---
+++ Assets/Scripts/Main/Road.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : Building
+{
+    public int Traffic { get; private set; }
+    public void Start()
+    {
+        Money = -200;
+        Traffic = 10;
+        AvailableSpace = -2;
+        Energy = -50;
+        Color = Color.gray;
+    }
+
+}
